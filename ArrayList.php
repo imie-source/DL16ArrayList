e6d33f76116247b7d38f228ed06af26be5d2f06f
Gabriel Block <gabriel.block@imie.fr>
2016-08-19 11:09:49 +0200

    functions implementation...

--- ArrayList.php
+++ ArrayList.php
@@ -14,7 +14,7 @@ class ArrayList{
         $txt = "[ ";
         $lastIndex = count($this->items) - 1;
         foreach($this->items as $key => $item){            
-            $txt .= '<span style="color: red">' .$item . '</span>';
+            $txt .= '<span style="color: red">' . $item . '</span>';
             if($key !== $lastIndex){
                 $txt .= ", ";
             }
@@ -29,34 +29,164 @@ class ArrayList{
 
     public function remove($index){
         $removedValue = null;
-        $keepedItems = [];
+        $keptItems = [];
 
         foreach($this->items as $key => $item){
             if($key !== $index){
-                $keepedItems[] = $item;
+                $keptItems[] = $item;
             }
             else{
                 $removedValue = $item;
             }
         }
 
-        $this->items = $keepedItems;
+        $this->items = $keptItems;
 
         return $removedValue;
     }
 
+    public function removeAll($value){
+        $nbRemoved = 0;
+        $keptItems = [];
+
+        foreach($this->items as $item){
+            if($item !== $value){
+                $keptItems[] = $item;
+            }
+            else{
+                $nbRemoved++;
+            }
+        }
+
+        $this->items = $keptItems;
+
+        return $nbRemoved;
+    }
+
+    public function get($index){
+        return isset($this->items[$index]) ? $this->items[$index] : null;
+    }
+
+    public function count(){
+        return count($this->items);
+    }
+
+    public function sizeof(){
+        return $this->count();
+    }
+
+    public function combine(ArrayList $list){
+        foreach($list->items as $item){
+            $this->add($item);
+        }
+
+        // for($ii = 0; $ii < $list->count(); $ii++){
+        //     $this->add($list->get($ii));
+        // }
+    }
+
+    public function chunk($size){
+        $chunks = [];
+        $chunk = [];
 
 
+        foreach($this->items as $item){
+            $chunk[] = $item;
+            if(count($chunk) === $size){
+                $chunks[] = $chunk;
+                $chunk = [];
+            }
+        }
+
+        if(count($chunk) !== 0){
+            $chunks[] = $chunk;
+        }
 
 
+        return $chunks;
 
+    }
 
+    public function fill($size, $value, $offset = 0){
+        if($offset < 0 || $offset > $this->count()){
+            $offset = $this->count();
+        }
 
+        for($ii = $offset; $ii < $offset + $size; $ii++){
+            $this->items[$ii] = $value;
+        }
+
+    }
+
+    public function isEmpty(){
+        return $this->count() === 0;
+        // return count($this->items) === 0;
+    }
+
+    public function pop(){
+        return $this->remove($this->count() - 1);
+    }
+
+    public function shift(){
+        return $this->remove(0);
+    }
     public function getItems(){
         return $this->items;
     }
 
     public function setItems($items){
         $this->items = $items;
+    }
+
+    public function unique(){
+        // $uniqueList = new ArrayList();
+        // foreach($this->items as $item){
+        //     if(!$uniqueList->includes($item)){
+        //         $uniqueList->add($item);
+        //     }
+        // }
+        // $this->items = $uniqueList->getItems();
+
+
+        $uniqueList = [];
+
+        foreach($this->items as $item){
+            $exists = false;
+            foreach($uniqueList as $keptItem){
+                if($item === $keptItem){
+                    $exists = true;
+                    break;
+                }
+            }
+            if(!$exists){
+                $uniqueList[] = $item;
+            }
+        }
+        $this->items = $uniqueList;
+    }
+
+    public function equals(ArrayList $list){
+        $equals = true;
+        foreach($this->items as $key => $item){
+            if($list->get($key) !== $item){
+                $equals = false;
+                break;
+            }
+        }
+
+        return $equals;
+
+        // return $this->items === $list->getItems();
+    }
+
+    public function includes($value){
+        $found = false;
+        foreach($this->items as $item){
+            if($item === $value){
+                $found = true;
+                break;
+            }
+        }
+        return $found;
     }
 }
--- index.php
+++ index.php
@@ -14,3 +14,44 @@ $removedElement = $list->remove(4);
 $list->debug();
 
 echo "L'élément supprimé est : " . $removedElement . "<br/>";
+
+$list2 = new ArrayList(["Bob", "John", "Alice", "Bob", "Gabriel"]);
+
+$list2->debug();
+
+$nbRemoved = $list2->removeAll("Bob");
+
+echo "$nbRemoved éléments supprimés";
+
+$list2->debug();
+
+echo $list2->get(45);
+
+$list->combine($list2);
+
+$list->debug();
+
+// var_dump($list->chunk(3));
+
+$list->fill(2, "PLOP", 3);
+
+$list->debug();
+
+$list3 = new ArrayList([1, 2]);
+$list3->debug();
+$list3->fill(5, 0, 1);
+$list3->debug();
+
+
+$list4 = new ArrayList([1,2,3,4,5]);
+$list5 = new ArrayList([1,2,3,4,5]);
+
+var_dump($list4->equals($list5));
+
+$list6 = new ArrayList([1,2,3,4,5,6,78,9,1,2,4,5,6,3,4,5,8,7,2,2,2,98]);
+
+$list6->unique();
+$list6->debug();
+
+
+
